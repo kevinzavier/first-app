f45caa2e72b080f4f81d2b80724cb73180b20a8a
Kevin Zavier <kzavier@ucsd.edu>
2017-02-21 17:43:29 -0800

    current works

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: '#fa-databinding',
   templateUrl: './databinding.component.html',
-  styleUrls: ['./databinding.component.css']
+  styleUrls: ['./databinding.component.css'],
 })
-export class DatabindingComponent implements OnInit {
+export class DatabindingComponent {
   stringInterpolation = 'This is string interpolation!';
   numberInterpolation = 2;
   constructor() { }
-
-  ngOnInit() {
-  }
 
   onTrue(){
     return true;
